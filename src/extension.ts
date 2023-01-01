00b9640f0438db51df19da63e627c0f9d31e6765
VladBrok <vlad.brok99@gmail.com>
2023-01-01 16:28:12 +0300

    broken commit

--- src/extension.ts
+++ src/extension.ts
@@ -46,4 +46,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(any) {}
