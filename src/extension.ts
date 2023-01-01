7106dafb50f155f7c00a48ec7e9383dc2fb57a7c
VladBrok <vlad.brok99@gmail.com>
2023-01-01 14:29:39 +0300

    init

---
+++ src/extension.ts
@@ -0,0 +1,42 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "helloworld" is now active!');
+
+  const name =
+    vscode.workspace.getConfiguration().get<string>("helloworld.targetName") ??
+    "";
+  vscode.window.showInformationMessage(name);
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable1 = vscode.commands.registerCommand(
+    "helloworld.helloVlad",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showWarningMessage(`Hello, ${name}!`);
+    }
+  );
+
+  const disposable2 = vscode.commands.registerCommand(
+    "helloworld.showCurTime",
+    () => {
+      vscode.window.showInformationMessage(
+        `Current time is: ${new Date().toLocaleTimeString()}`
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable1, disposable2);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
