731b953dffdf0942728ec1f17ca03edae8dd1533
VladBrok <vlad.brok99@gmail.com>
2023-02-26 20:02:03 +0300

    wo

--- src/extension.ts
+++ src/extension.ts
@@ -2,117 +2,43 @@ import { nanoid } from "nanoid";
 import * as vscode from "vscode";
 import { NodeDependenciesProvider } from "./tree-view";
 
+const randomBetween = (minInclusive: number, maxInclusive: number) => {
+  return (
+    Math.floor(Math.random() * (maxInclusive - minInclusive + 1)) + minInclusive
+  );
+};
+
 export function activate(context: vscode.ExtensionContext) {
-  const p = new NodeDependenciesProvider(context);
+  const key = "my_synced_key";
+  context.globalState.setKeysForSync([key]);
 
-  ////////////////////////
-  const name =
-    vscode.workspace.getConfiguration().get<string>("helloworld.targetName") ??
-    "";
+  const entries: [string, number][] = Array(randomBetween(3, 20))
+    .fill(0)
+    .map((_, i) => [`random_value_${i}:`, Math.random()]);
+  const data = Object.fromEntries(entries);
 
-  const disposable00 = vscode.commands.registerCommand(
-    "helloworld.saveSelectedCodeFragment",
-    () => {
-      const showNoTextMsg = () =>
-        vscode.window.showInformationMessage(
-          "Select a piece of code in the editor to save it as a fragment."
-        );
+  context.globalState.update(key, data).then(() => {
+    vscode.window.showInformationMessage(
+      `state: ${JSON.stringify(context.globalState.get(key), null, 2)}`
+    );
+  });
 
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        showNoTextMsg();
-        return;
-      }
+  vscode.commands.registerCommand("showState", () => {
+    vscode.window.showInformationMessage(
+      `state: ${JSON.stringify(context.globalState.get(key), null, 2)}`
+    );
+  });
 
-      editor.edit(() => {
-        const content = editor.document.getText(editor.selection);
+  vscode.commands.registerCommand("updateState", () => {
+    const entries: [string, number][] = Array(randomBetween(3, 20))
+      .fill(0)
+      .map((_, i) => [`random_value_${i}:`, Math.random()]);
+    const data = Object.fromEntries(entries);
 
-        if (content.length < 1) {
-          showNoTextMsg();
-          return;
-        }
-
-        const config = vscode.workspace.getConfiguration("helloworld");
-
-        const defaultLabel = content.substring(0, 100);
-
-        // TODO: add config option
-        // if (config.get("askForNameOnCreate")) {
-        if (true) {
-          const opt: vscode.InputBoxOptions = {
-            ignoreFocusOut: false,
-            placeHolder: "Code Fragment Name",
-            prompt: "Give the fragment a name...",
-            value: defaultLabel,
-          };
-
-          vscode.window.showInputBox(opt).then((label) => {
-            p.saveNewCodeFragment(content, label);
-          });
-        } else {
-          // fragmentManager.saveNewCodeFragment(content, defaultLabel);
-        }
-      });
-    }
-  );
-
-  const disposable0 = vscode.commands.registerCommand(
-    "helloworld.insertCodeFragment",
-    (id: string) => {
-      if (!id) {
-        vscode.window.showInformationMessage(
-          "Insert a code fragment into the editor by clicking on it in the Code Fragments view."
-        );
-      }
-
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        vscode.window.showInformationMessage(
-          "Open a file in the editor to insert a fragment."
-        );
-        return;
-      }
-
-      const content = p.getFragmentContent(id);
-
-      if (content) {
-        editor.edit((builder) => {
-          builder.insert(editor.selection.start, content);
-        });
-      }
-    }
-  );
-
-  const disposable1 = vscode.commands.registerCommand(
-    "helloworld.helloVlad",
-    () => {
-      vscode.window.showWarningMessage(`Hello, ${name}!`);
-    }
-  );
-
-  const disposable2 = vscode.commands.registerCommand(
-    "helloworld.showCurTime",
-    () => {
+    context.globalState.update(key, data).then(() => {
       vscode.window.showInformationMessage(
-        `Current time is: ${new Date().toLocaleTimeString()}`
+        `state: ${JSON.stringify(context.globalState.get(key), null, 2)}`
       );
-    }
-  );
-
-  const disposable3 = vscode.commands.registerCommand(
-    "helloworld.generateId",
-    () => {
-      vscode.window.showInformationMessage(`Unique id: ${nanoid()}`);
-    }
-  );
-
-  context.subscriptions.push(
-    disposable00,
-    disposable0,
-    disposable1,
-    disposable2,
-    disposable3
-  );
+    });
+  });
 }
-
-export function deactivate() {}
